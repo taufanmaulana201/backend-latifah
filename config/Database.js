5faf3f088e6cba85ae832516fa120c70ef28d7bd
taufan <taufanmaulana201201@gmail.com>
2023-06-20 19:46:48 +0700

    setting config

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,12 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_latifah", "postgres", "database", {
-  host: "localhost",
+// const db = new Sequelize("db_latifah", "postgres", "database", {
+//   host: "localhost",
+//   dialect: "postgres",
+// });
+const db = new Sequelize("railway", "postgres", "L9kY6Rx9BsT2dZQuTpXc", {
+  host: "containers-us-west-209.railway.app",
+  port: 6626,
   dialect: "postgres",
 });
 
